f18d2b6f58f0f12ea3919b9c740c2efccfe4a251
anshugpt <138896567+anshugpt@users.noreply.github.com>
2023-10-16 21:18:31 +0530

    update app.js

--- app.js
+++ app.js
@@ -34,3 +34,16 @@ let even = num1.filter((n) => n%2==0);
 // -> even variable have -> [2, 4, 6, 8]
 
 
+//* Every 
+// return true if every elements of array gives true for some function. else return false
+// all condition need to have true value so the overall output will be true
+
+let arr1 = [2, 4, 6, 8]
+console.log(arr1.every((n) => n%2==0));  //* -> return true
+
+//* Some 
+// same as every but like or if some of are true then overall output will be true
+
+[1, 2, 3, 4].some((n) => n%2==0); //* -> return -> true
+
+
